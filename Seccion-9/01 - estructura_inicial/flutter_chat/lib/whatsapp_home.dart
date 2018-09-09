da4c7cb442b1647865b4a297cc3b28eeea5b9d3f
Rodrigo <rodrigo.moralesv@hotmail.com>
2018-09-09 20:38:57 -0300

    estructura inicial

---
+++ Seccion-9/01 - estructura_inicial/flutter_chat/lib/whatsapp_home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WhatsAppHome extends StatefulWidget {
+  @override
+  _WhatsAppHomeState createState() => new _WhatsAppHomeState();
+ }
+class _WhatsAppHomeState extends State<WhatsAppHome> {
+  @override
+  Widget build(BuildContext context) {
+   return new Container(
+  
+   );
+  }
+}
